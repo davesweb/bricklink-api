128365d6b127f0b4901cf797b57bc1922cadcc8b
davesweb <davesweb@gmail.com>
2021-08-09 19:48:54 +0200

    Implement Item value object

--- src/ValueObjects/Item.php
+++ src/ValueObjects/Item.php
@@ -5,10 +5,20 @@ namespace Davesweb\BrinklinkApi\ValueObjects;
 class Item
 {
     public function __construct(
-        public ?string $no = null,
+        public ?string $number = null,
         public ?string $name = null,
         public ?string $type = null,
         public ?int $categoryId = null,
-    ) {
-    }
+        public ?string $alternateNumber = null,
+        public ?string $imageUrl = null,
+        public ?string $thumbnailUrl = null,
+        public ?float $weight = null,
+        public ?float $dimX = null,
+        public ?float $dimY = null,
+        public ?float $dimZ = null,
+        public ?int $yearReleased = null,
+        public ?string $description = null,
+        public ?bool $isObsolete = null,
+        public ?string $languageCode = null,
+    ) {}
 }
