a375a97acc9805dfba58363da5e0c3557a25b439
davesweb <davesweb@gmail.com>
2021-08-09 20:40:45 +0200

    Implement shipping method value object

--- src/ValueObjects/ShippingMethod.php
+++ src/ValueObjects/ShippingMethod.php
@@ -4,4 +4,14 @@ namespace Davesweb\BrinklinkApi\ValueObjects;
 
 class ShippingMethod
 {
+    public function __construct(
+        public ?int $methodId = null,
+        public ?string $name = null,
+        public ?string $note = null,
+        public ?bool $insurance = null,
+        public ?bool $isDefault = null,
+        public ?string $area = null,
+        public ?bool $isAvailable = null,
+    ) {
+    }
 }
