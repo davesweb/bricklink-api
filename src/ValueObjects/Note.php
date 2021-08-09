e144edc27623e8541132591558199aadd6da28cc
davesweb <davesweb@gmail.com>
2021-08-09 20:43:35 +0200

    Implement member value objects

--- src/ValueObjects/Note.php
+++ src/ValueObjects/Note.php
@@ -2,6 +2,15 @@
 
 namespace Davesweb\BrinklinkApi\ValueObjects;
 
+use DateTime;
+
 class Note
 {
+    public function __construct(
+        public ?int $noteId = null,
+        public ?string $username = null,
+        public ?string $text = null,
+        public ?DateTime $dateNoted = null,
+    ) {
+    }
 }
--- src/ValueObjects/Rating.php
+++ src/ValueObjects/Rating.php
@@ -4,4 +4,9 @@ namespace Davesweb\BrinklinkApi\ValueObjects;
 
 class Rating
 {
+    public function __construct(
+        public ?string $username = null,
+        public ?iterable $rating = null,
+    ) {
+    }
 }
