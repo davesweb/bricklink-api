b0957944a63d4ebccafa779f8e14e83f28fb9b3a
davesweb <davesweb@gmail.com>
2021-08-09 20:14:38 +0200

    Implement category value object

--- src/ValueObjects/Category.php
+++ src/ValueObjects/Category.php
@@ -4,4 +4,9 @@ namespace Davesweb\BrinklinkApi\ValueObjects;
 
 class Category
 {
+    public function __construct(
+        public ?int $categoryId = null,
+        public ?string $categoryName = null,
+        public ?int $parentId = null,
+    ){}
 }
