e1c25dfec196fe3df19e38f2e5585942c7c581db
davesweb <davesweb@gmail.com>
2021-08-09 20:13:20 +0200

    Implement color value object

--- src/ValueObjects/Color.php
+++ src/ValueObjects/Color.php
@@ -4,4 +4,10 @@ namespace Davesweb\BrinklinkApi\ValueObjects;
 
 class Color
 {
+    public function __construct(
+        public ?int $colorId = null,
+        public ?string $colorName = null,
+        public ?string $colorCode = null,
+        public ?string $colorType = null,
+    ){}
 }
