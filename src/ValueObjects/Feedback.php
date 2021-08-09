879d26507901f95fd47b876a86c565b977371bf2
davesweb <davesweb@gmail.com>
2021-08-09 20:01:57 +0200

    Implement Feedback value object

--- src/ValueObjects/Feedback.php
+++ src/ValueObjects/Feedback.php
@@ -2,6 +2,20 @@
 
 namespace Davesweb\BrinklinkApi\ValueObjects;
 
+use DateTime;
+
 class Feedback
 {
+    public function __construct(
+        public ?int $feedbackId = null,
+        public ?int $orderId = null,
+        public ?string $from = null,
+        public ?string $to = null,
+        public ?DateTime $dateRated = null,
+        public ?int $rating = null,
+        public ?string $ratingOfBs = null,
+        public ?string $comment = null,
+        public ?string $reply = null,
+    ) {
+    }
 }
